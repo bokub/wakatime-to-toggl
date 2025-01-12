bc52008faad9986b1953b671972c0f616bf163e3
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-01-12 22:59:05 +0100

    fix(toggl): unable to add new projects

--- src/toggl.js
+++ src/toggl.js
@@ -48,6 +48,7 @@ export async function createProject(name, workspaceId, apiKey) {
       `workspaces/${workspaceId}/projects`,
       {
         name: name,
+        active: true,
       },
       {
         auth: {
